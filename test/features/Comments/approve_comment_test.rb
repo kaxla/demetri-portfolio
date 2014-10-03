ce73197e1b7430ab07207e3c1e774248f80725dd
kaxla <kaylas.email.address@gmail.com>
2014-10-03 11:56:53 -0700

    write initial comment tests, generate comment resource

---
+++ test/features/Comments/approve_comment_test.rb
@@ -0,0 +1,15 @@
+# scenario "Unapproved comments don't display for users" do
+
+# end
+
+# scenario "Editor can approve comments" do
+
+# end
+
+# scenario "Author can approve comments" do
+
+# end
+
+# scenario "Users can't approve comments" do
+
+# end
---
+++ test/features/Comments/create_comment_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+feature "comments" do
+  scenario "signed-in user can create comment" do
+
+  end
+
+  scenario "unsigned in user can't create comment" do
+
+  end
+end
